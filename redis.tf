2b6fa1d3368334331f1a0141d20ad0e67f3e6315
Sush <sushcloud12@gmail.com>
2023-11-08 22:57:06 +0000

    i

--- redis.tf
+++ redis.tf
@@ -6,7 +6,7 @@ resource "aws_elasticache_cluster" "redis" {
   node_type            = "cache.t3.medium"
   num_cache_nodes      = 1
   parameter_group_name = aws_elasticache_parameter_group.default.name
-  subnet_group_name = aws_redis_subnet_group.redis.name
+  subnet_group_name = aws_elasticache_subnet_group.redis.name
   security_group_ids = [aws_security_group.allows_redis.id]
   engine_version       = "6.x"
   port                 = 6379
@@ -21,7 +21,7 @@ resource "aws_elasticache_parameter_group" "default" {
 }
 
 # Creates redis subnet group
-resource "aws_redis_subnet_group" "redis" {
+resource "aws_elasticache_subnet_group" "redis" {
   name       = "roboshop-${var.ENV}-redis-subnet-group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
   tags = {
