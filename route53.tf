a31b47144baf2fde9e049ffaa624701926c6c97c
Sush <sushcloud12@gmail.com>
2023-11-17 00:10:43 +0000

    i

--- route53.tf
+++ route53.tf
@@ -1,15 +1,18 @@
-# adding cname record for redis endpoint
-# in redis if there 2 nodes then there will be two end points we have decide 
-# which endpoint we use
-# resource "aws_route53_record" "redis_dns" {
-#   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-#   name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
-#   type    = "CNAME"
-#   ttl     = 10
-#   records = [aws_db_instance.mysql.address]
-# }
+##adding cname record for redis endpoint
+##in redis if there 2 nodes then there will be two end points we have decide 
+##which endpoint we use
 
-output "redis" {
-value = aws_elasticache_cluster.redis
-  
+resource "aws_route53_record" "redis_dns" {
+  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  name    = "redis-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
+  type    = "CNAME"
+  ttl     = 10
+  records = [aws_elasticache_cluster.redis.cache_nodes.0.address]
 }
+
+
+##for debug to get the node details
+# output "redis" {
+# value = aws_elasticache_cluster.redis
+  
+# }
